ae97c17fe2ac9b495da6e7b65629cc13a4ddae7b
Benjamin <bunyamin.cuci@skiff.com>
2024-10-27 19:13:48 +0300

    Footer organized. Home page desktop layout built.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,10 +7,12 @@ export default function Home() {
             <div className="fade-image">
                 <img src="/images/moodboard.png" />
             </div>
-            <h2>Code Faster</h2>
-            <h3>with intuitive React components</h3>
-            <p>I, Benjamin Cuci, built react components that you can easily add to your React projects as components.</p>
-            <Link className="discover" to="/howto">Discover</Link>
+            <div className="home-text">
+                <h2>Code Faster</h2>
+                <h3>with intuitive React components</h3>
+                <p>I, Benjamin Cuci, built react components that you can easily add to your React projects as components.</p>
+                <Link className="discover" to="/howto">Discover</Link>
+            </div>
         </div>
     )
 }
