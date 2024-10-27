be0f0159afa6117c6ce49ab9811bb6f054a26625
Benjamin <bunyamin.cuci@skiff.com>
2024-10-27 19:41:30 +0300

    Responsiveness to the screen width improved, footer layout improved.

--- src/layouts/Footer.jsx
+++ src/layouts/Footer.jsx
@@ -4,7 +4,7 @@ export default function Footer() {
     return (
         <footer>
             <div className="footer-container">
-                <p>&copy; 2024 Benjamin Cuci. All rights reserved.</p>
+                <p>&copy; 2024 Custom-Components. All rights reserved.</p>
                 <div className="footer-links">
                     <a href="https://www.linkedin.com/in/benjamin-cuci/"><i className="fa-brands fa-linkedin-in"></i></a>
                     <a href="https://github.com/Bencuci"><i className="fa-brands fa-square-github"></i></a>
