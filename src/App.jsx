c8ce96a7343eea8cb05a1bc56d22812246f84ee0
Benjamin <bunyamin.cuci@skiff.com>
2024-11-01 20:36:06 +0300

    logo modified

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import MainLayout from "./layouts/MainLayout"
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Gallery from "./pages/Gallery"
 
 export default function App() {
     return (
@@ -11,6 +12,7 @@ export default function App() {
                 <Route path="/" element={<MainLayout />} >
                     <Route index element={<Home />} />
                     <Route path="about" element={<About />} />
+                    <Route path="gallery" element={<Gallery />} />
                 </Route>
             </Routes>
         </Router>
